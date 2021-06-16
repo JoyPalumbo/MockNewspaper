8b6f7bc707b616a256a56546769e02c65722d9ba
Joy <greeniejoy@gmail.com>
2021-06-16 00:29:48 -0500

    Added artcie two with image

---
+++ src/components/ArticleTwo.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+ <v-img  max-height="150"
+  max-width="250"
+ :src="article[7].urlToImage"></v-img>
+<h5 sm8>
+  {{article[7].title}}
+</h5>
+
+<!-- <h6 sm2>{{article[11].description}}</h6> -->
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ArticleTwo",
+  props: ["article"],
+};
+</script>
