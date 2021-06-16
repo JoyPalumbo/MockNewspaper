9da41b044f3023b55e27c7f35b5261a614fce79b
Joy <greeniejoy@gmail.com>
2021-06-16 00:30:07 -0500

    Added article three with image

---
+++ src/components/ArticleThree.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+ <v-img  max-height="150"
+  max-width="250"
+ :src="article[9].urlToImage"></v-img>
+<h5 sm8>
+  {{article[9].title}}
+</h5>
+<!-- <h6 sm2>{{article[12].description}}</h6> -->
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ArticleTwo",
+  props: ["article"],
+};
+</script>
